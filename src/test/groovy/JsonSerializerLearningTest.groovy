11b7e0528d3bd6a63793ab8df46137b2e9cd12a3
Ron Kurr <kurr@kurron.org>
2013-07-22 10:37:02 -0400

    Made a parameterized test.

--- src/test/groovy/JsonSerializerLearningTest.groovy
+++ src/test/groovy/JsonSerializerLearningTest.groovy
@@ -7,19 +7,24 @@ import spock.lang.Specification
  * A learning test just to see how the JSON structures might look.
  */
 class JsonSerializerLearningTest extends Specification {
-    def 'bob'()
+    def 'dump JSON structures'( def builder, def unimportant )
     {
         given: 'a JSON serializer'
         ObjectMapper mapper = new ObjectMapper()
         mapper.configure( SerializationFeature.INDENT_OUTPUT, true )
         mapper.configure( DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false )
-        def sut = new DailyAggregateBuilder().build()
 
         when: 'an object is serialized into JSON'
-        String json = mapper.writer().writeValueAsString( sut )
+        def sut = builder.build()
+        String json = mapper.writer().writeValueAsString(sut)
 
         then: 'printout JSON'
+        println sut.class.simpleName
         println "$json"
         true
+
+        where:
+        builder                     |  unimportant
+        new DailyAggregateBuilder() |  'bob'
     }
 }
