2be4f28beaefe74eb43a8a92296981ee63c7c64e
Ron Kurr <kurr@kurron.org>
2013-07-30 14:54:53 -0400

    Created basic Spring Data Mongo objects in the context.

--- src/test/groovy/org/kurron/domain/DataPopulationLearningTestConfiguration.groovy
+++ src/test/groovy/org/kurron/domain/DataPopulationLearningTestConfiguration.groovy
@@ -1,12 +1,32 @@
 package org.kurron.domain
 
+import com.mongodb.Mongo
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ImportResource
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor
+import org.springframework.data.mongodb.core.MongoFactoryBean
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.MongoTemplate
 
 /**
  * Spring context for the learning test.
  */
+@SuppressWarnings("GrMethodMayBeStatic")
 @Configuration
 @ImportResource( 'classpath:/org/kurron/domain/DataPopulationLearningTest-context.xml' )
 class DataPopulationLearningTestConfiguration {
+    public @Bean MongoOperations mongoTemplate(Mongo mongo) {
+        new MongoTemplate( mongo, 'test' )
+    }
+
+    public @Bean MongoFactoryBean mongo() {
+        MongoFactoryBean mongo = new MongoFactoryBean();
+        mongo.host = 'mongo'
+        return mongo
+    }
+
+    public @Bean PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor() {
+        new PersistenceExceptionTranslationPostProcessor()
+    }
 }
