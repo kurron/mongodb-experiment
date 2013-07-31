5a0f91f210dbd471dc1d842f56eddbee75cd7bd1
Ron Kurr <kurr@kurron.org>
2013-07-31 10:14:10 -0400

    We now query the database to find out how many documents we inserted.

--- src/test/groovy/org/kurron/domain/DataPopulationLearningTest.groovy
+++ src/test/groovy/org/kurron/domain/DataPopulationLearningTest.groovy
@@ -3,8 +3,10 @@ package org.kurron.domain
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
+import static org.springframework.data.mongodb.core.query.Criteria.*
 
 /**
  * A learning test just to see how MongoDB might handle large amounts of data.
@@ -21,42 +23,36 @@ class DataPopulationLearningTest extends Specification {
         assert template != null
         DailyUserAggregateBuilder builder = new DailyUserAggregateBuilder()
 
-        when: 'an object is inserted into the database'
+        and: 'an empty collection'
         if ( template.collectionExists( DailyUserAggregate ) ) {
             template.dropCollection( DailyUserAggregate )
         }
         template.createCollection( DailyUserAggregate )
 
-        final int NUMBER_OF_USERS = 1
-        final int NUMBER_OF_YEARS = 2
+        when: 'data is inserted into the database'
+        final int NUMBER_OF_USERS = 10
+        final int NUMBER_OF_YEARS = 10
         final int NUMBER_OF_DAYS = 365
         log.debug( "Creating $NUMBER_OF_USERS users" )
-        1.upto( NUMBER_OF_USERS ) {
-            log.debug( "Creating $NUMBER_OF_YEARS years" )
+        1.upto( NUMBER_OF_USERS ) { student ->
             DailyUserAggregate data = builder.build()
+            data.node = 'ONE'
+            data.organization = 'ONE'
             data.student.totalLessonSessionCount = 1
             1.upto( NUMBER_OF_YEARS ) { year ->
-                log.debug( "Creating $NUMBER_OF_DAYS days" )
                 1.upto( NUMBER_OF_DAYS ) { day ->
                     data.dateCode = day + (NUMBER_OF_DAYS * (year - 1))
                     data.id = UUID.randomUUID()
                     log.debug( "Date code for year $year day $day is $data.dateCode" )
-                    log.info( "Inserting student $data.student.studentID for day $data.dateCode into database as record $data.id" )
+                    log.info( "Inserting student $student for year $year day $day into database as record $data.id" )
                     template.insert( data )
                 }
             }
         }
 
-        then: 'we should find it'
-        List<DailyUserAggregate> results = template.findAll( DailyUserAggregate )
-        assert !results.empty
-        log.debug "Found $results.size in the database"
-
-/*
-        results.each {
-            log.debug( it.toString() )
-        }
-*/
-        true
+        then: 'we should be able to count the inserted documents'
+        long count = template.count( new Query( where( 'node' ).is( 'ONE' ) ), DailyUserAggregate )
+        log.info "Found $count in the database"
+        assert count == NUMBER_OF_USERS * NUMBER_OF_YEARS * NUMBER_OF_DAYS
     }
 }
